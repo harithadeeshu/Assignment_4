05d8fe1c49e1b896647fce415028a41d635db426
harithadeeshu <131469436+harithadeeshu@users.noreply.github.com>
2023-05-26 18:18:17 +0530

    Add files via upload

---
+++ a4q3.py
@@ -0,0 +1,9 @@
+#Write a Python program to square the elements of a list using map() function.
+
+def square_num(n):
+  return n * n
+nums = [4, 5, 2, 9]
+print("Original List: ",nums)
+result = map(square_num, nums)
+print("Square the elements of the said list using map():")
+print(list(result))
